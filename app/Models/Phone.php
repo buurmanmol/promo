e12e8bf79c94abe5adcd1ac0c8004d1bfc7b4a51
Anton Dolganov <a.m.dolganov@student.rug.nl>
2021-07-06 14:08:44 +0200

    database fixed and start

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\AbstractList;
 
 class Phone extends Model
 {
-    use HasFactory;
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        "id",
+
+    ];
+    public function phone(){
+        return $this->hasOne(Phone::class);
+    }
 }
