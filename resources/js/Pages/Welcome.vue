eeeeda0ba8ba341271aa874dbb990d7888b9806c
buurmanmol <thomas-mol@online.nl>
2021-07-11 11:53:35 +0200

    Home page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -29,13 +29,13 @@
     <div class="bg-white">
         <div class="relative overflow-hidden">
             <Popover as="header" class="relative">
-                <div class="bg-gray-900 pt-6">
+                <div class="bg-gray-800 pt-6">
                     <nav class="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6" aria-label="Global">
                         <div class="flex items-center flex-1">
                             <div class="flex items-center justify-between w-full md:w-auto">
                                 <a href="#">
                                     <span class="sr-only">Workflow</span>
-                                    <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg" alt="" />
+                                    <img class="h-8 w-auto sm:h-10" src=../../../v2.png />
                                 </a>
                                 <div class="-mr-2 flex items-center md:hidden">
                                     <PopoverButton class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
@@ -50,10 +50,10 @@
                         </div>
                         <div class="hidden md:flex md:items-center md:space-x-6">
                             <a href="#" class="text-base font-medium text-white hover:text-gray-300">
-                                Log in
+                                Inloggen
                             </a>
                             <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700">
-                                Start free trial
+                                Offerte Aanvragen
                             </a>
                         </div>
                     </nav>
@@ -89,35 +89,37 @@
                 </transition>
             </Popover>
             <main>
-                <div class="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+                <div class="pt-10 bg-gray-800 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
                     <div class="mx-auto max-w-7xl lg:px-8">
                         <div class="lg:grid lg:grid-cols-2 lg:gap-8">
                             <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
                                 <div class="lg:py-24">
                                     <a href="#" class="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
-                                        <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">We're hiring</span>
-                                        <span class="ml-4 text-sm">Visit our careers page</span>
+                                        <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">Interesse?</span>
+                                        <span class="ml-4 text-sm">Vraag een offerte aan!</span>
                                         <ChevronRightIcon class="ml-2 w-5 h-5 text-gray-500" aria-hidden="true" />
                                     </a>
-                                    <h1 class="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                                        <span class="block">A better way to</span>
-                                        <span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 block">ship web apps</span>
+                                    <h1 class="mt-4 text-lg tracking-tight font-extrabold text-white sm:mt-5 sm:text-4xl lg:mt-6 xl:text-4xl">
+                                        <span class="block">Een servicabonnement</span>
+                                        <span class="bg-clip-text bg-gradient-to-r from-teal-200 to-cyan-400 block">Voor uw zakelijke apparaten</span>
                                     </h1>
                                     <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
+                                        Door gebruik te maken van PromoFix zorgt u ervoor dat al uw zakelijke apparaten binnen 24 uur gerepareerd kunnen worden.
+                                        Meld uw reparaties gemakkelijk via uw persoonlijke dashboard aan.
                                     </p>
                                     <div class="mt-10 sm:mt-12">
                                         <form action="#" class="sm:max-w-xl sm:mx-auto lg:mx-0">
                                             <div class="sm:flex">
                                                 <div class="min-w-0 flex-1">
-                                                    <label for="email" class="sr-only">Email address</label>
-                                                    <input id="email" type="email" placeholder="Enter your email" class="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900" />
+                                                    <label for="email" class="sr-only">Uw emailadres.hier.</label>
+                                                    <input id="email" type="email" placeholder="Uw emailadres..." class="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900" />
                                                 </div>
                                                 <div class="mt-3 sm:mt-0 sm:ml-3">
-                                                    <button type="submit" class="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">Start free trial</button>
+                                                    <button type="submit" class="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-200 to-cyan-800 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-100">Meer informatie</button>
                                                 </div>
                                             </div>
-                                            <p class="mt-3 text-sm text-gray-300 sm:mt-4">Start your free 14-day trial, no credit card necessary. By providing your email, you agree to our <a href="#" class="font-medium text-white">terms or service</a>.</p>
+                                            <p class="mt-3 text-sm text-gray-300 sm:mt-4">Door uw emailadres hier in te vullen voor meer informatie, gaat u
+                                                akkoord met onze <a href="#" class="font-medium text-white">algemene voorwaarden</a>.</p>
                                         </form>
                                     </div>
                                 </div>
@@ -136,12 +138,12 @@
                 <div class="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
                     <div class="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
                         <div>
-                            <h2 class="text-base font-semibold tracking-wider text-cyan-600 uppercase">Serverless</h2>
+                            <h2 class="text-base font-semibold tracking-wider text-cyan-600 uppercase">Dashboard</h2>
                             <p class="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-                                No server? No problem.
+                                Schade? Geen probleem.
                             </p>
                             <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-                                Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.
+                                Via uw persoonlijke dashboard krijgt u een overzicht van alles wat u nodig hebt. Ook meldt u met een paar klikken een reparatie aan!
                             </p>
                         </div>
                         <div class="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
