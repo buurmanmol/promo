dba436468faf5f8c3c0f93a506c0dda7db6c6349
Wouter Moelker <wouter.moelker@hotmail.com>
2021-07-22 11:50:46 +0200

    invoice id repair

--- app/Models/Repair.php
+++ app/Models/Repair.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Repair extends Model
 {
     use HasFactory;
-    protected $fillable = ['price','device_id', 'product_type_id', 'user_id', 'comment', 'is_repaired'];
+    protected $fillable = ['price','invoice_id','device_id', 'product_type_id', 'user_id', 'comment', 'is_repaired'];
 
     public function productType()
     {
@@ -23,5 +23,10 @@ class Repair extends Model
     public function user()
     {
         return $this->hasOne(User::class);
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
     }
 }
